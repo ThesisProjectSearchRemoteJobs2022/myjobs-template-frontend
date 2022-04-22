a437472f78d956b10a2ed3a1cb523ee1ddf150f3
Roger Colquehuanca <rogcolquehuancac@gmail.com>
2022-04-21 19:52:31 -0500

    change env en Netlify portal

--- src/components/JobsList.jsx
+++ src/components/JobsList.jsx
@@ -55,12 +55,12 @@ const JobsList = () => {
                           {/* stroke-linecap="round" stroke-linejoin="round" */}
                             <button onClick={()=>submit()} className="outline-none focus:outline-none"><svg className=" w-5 text-gray-600 h-5 cursor-pointer" fill="none"  strokeWidth="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg></button>
                             <input onChange={(e)=> handle(e)} value={jobSearch} type="search" name="jobSearch" id="jobSearch" placeholder="search for images" x-model="q" className="w-full pl-4 text-sm outline-none focus:outline-none bg-transparent" />
-                            <div className="select">
+                            {/* <div className="select">
                               <select name="" id="" x-model="image_type" className="text-sm outline-none focus:outline-none bg-transparent">
                                 <option value="all" defaultValue>All</option>
                                 <option value="photo">Photo</option>
                               </select>
-                            </div>
+                            </div> */}
                           </div>
                         
                       </div>
